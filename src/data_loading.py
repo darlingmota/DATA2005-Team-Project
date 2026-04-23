4c515d15ccc21cb75108dba7777fd9b08dacd524
darlingmota <darlingmotta@gmail.com>
2026-04-23 20:31:45 +0100

    error checking for opening raw data

--- src/data_loading.py
+++ src/data_loading.py
@@ -9,3 +9,9 @@ def load_raw_data(filepath):
         print(f" loaaded {len(df):,} rows and {len(df.columns)} columns")
         return df
         
+    except FileNotFoundError:
+        print(f"error no file found")
+        return None
+    except Exception as e:
+        print(f"error loading file pls try again: {e}")
+        return None
